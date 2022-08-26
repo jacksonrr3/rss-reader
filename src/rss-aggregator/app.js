29e2cec31961ea8ad09160538f32fa985d990ce9
e.zaborshchikov <e.zaborshchikov@yandex.ru>
2022-08-26 15:34:32 +0300

    add state and elements

--- src/rss-aggregator/app.js
+++ src/rss-aggregator/app.js
@@ -1,1 +1,33 @@
-export default async () => { };
+
+// const render = ()
+
+export default () => {
+  const elements = {
+    form: document.querySelector('.rss-form'),
+    fields: {
+      urlInput: document.getElementById('url-input'),
+    },
+    feedback: document.querySelector('.feedback'),
+  };
+
+  const state = {
+    form: {
+      valid: true,
+      prosessState: 'filling',
+      processError: null,
+      errors: {},
+    },
+    rss: [],
+  };
+
+  elements.form.addEventListener('submit', (event) => { 
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    const url = formData.get('url');
+    if (isValudUrl(url)) { 
+      
+    }
+  });
+
+  // render(state, elements);
+};
